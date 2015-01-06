9b83785ea74eb22aa8bb9f0a081cbce06a7eab61
TomGroombridge <groombridgetom@gmail.com>
2015-01-06 10:59:42 +0000

    added capybara and a failing feature test

---
+++ spec/features/create_product_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+	describe 'create product' do 
+		
+	context 'given a name and description' do 
+
+		it 'should display the new course in the list of courses' do
+			visit "/product/new"
+			fill_in 'Name', with: 'Pallet'
+			fill_in 'Description', with: "A full Pallet"
+			click_button 'Create Product'
+
+			expect(current_path).to eq '/products'
+			expect(page).to have_content 'A full Pallet'
+		end
+
+	end
+
+end
